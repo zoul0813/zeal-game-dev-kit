b090dab4ba17ec89b7ad43033bd11f3c7ca43e1f
David Higgins <zoul0813@me.com>
2025-01-22 09:29:41 -0500

    * added "tilemap_fill" * added tilemap comments

--- include/tilemap.h
+++ include/tilemap.h
@@ -1,9 +1,13 @@
 #include <stdint.h>
+#include <zvb_gfx.h>
 #include <zvb_hardware.h>
 
 #ifndef TILEMAP_H
 #define TILEMAP_H
 
+/**
+ * set the current tilemap `x` offset on `layer`
+ */
 static void tilemap_scroll_x(uint8_t layer, uint16_t value)
 {
     if (layer == 0) {
@@ -15,6 +19,9 @@ static void tilemap_scroll_x(uint8_t layer, uint16_t value)
     }
 }
 
+/**
+ * set the current tilemap `y` offset on `layer`
+ */
 static void tilemap_scroll_y(uint8_t layer, uint16_t value)
 {
     if (layer == 0) {
@@ -26,12 +33,18 @@ static void tilemap_scroll_y(uint8_t layer, uint16_t value)
     }
 }
 
+/**
+ * set the current tilemap `x` and `y` offset on `layer`
+ */
 static void tilemap_scroll(uint8_t layer, uint16_t x, uint16_t y)
 {
     tilemap_scroll_x(layer, x);
     tilemap_scroll_y(layer, y);
 }
 
+/**
+ * get the current tilemap `layer` x offset
+ */
 static uint16_t tilemap_scroll_get_x(uint8_t layer)
 {
     uint16_t value;
@@ -45,6 +58,9 @@ static uint16_t tilemap_scroll_get_x(uint8_t layer)
     return value;
 }
 
+/**
+ * get the current tilemap `layer` y offset
+ */
 static uint16_t tilemap_scroll_get_y(uint8_t layer)
 {
     uint16_t value;
@@ -58,5 +74,17 @@ static uint16_t tilemap_scroll_get_y(uint8_t layer)
     return value;
 }
 
+/**
+ * Fill the tilemap with `tile` from x,y to x+width,y+height with `tile` on `layer`
+ */
+static void tilemap_fill(gfx_context *ctx, uint8_t tile, uint8_t layer, uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
+    for(uint8_t j = 0; j < height; j++) {
+        uint8_t line[80];
+        for(uint8_t i = 0; i < width; i++) {
+            line[i] = tile;
+        }
+        gfx_tilemap_load(ctx, line, width, layer, x, y + j);
+    }
+}
 
 #endif
