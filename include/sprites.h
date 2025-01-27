a30bbaf0aafa82477b589ab95fa1624ae9f7fde5
David Higgins <zoul0813@me.com>
2025-01-27 15:21:07 -0500

    * add sprite arena

--- include/sprites.h
+++ include/sprites.h
@@ -1,7 +1,14 @@
+#include <stdint.h>
+#include <zvb_gfx.h>
+#include <zvb_sprite.h>
+
 #ifndef SPRITES_H
 #define SPRITES_H
 
 #define SPRITE_HEIGHT       16
 #define SPRITE_WIDTH        16
 
+gfx_error sprites_register_arena(gfx_sprite* arena, uint8_t size);
+gfx_sprite* sprites_register(gfx_sprite sprite);
+gfx_error sprites_render(gfx_context* ctx);
 #endif
---
+++ src/sprites.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "sprites.h"
+
+static gfx_sprite *ARENA;
+static uint8_t ARENA_SIZE;
+static uint8_t registered;
+
+gfx_error sprites_register_arena(gfx_sprite* arena, uint8_t size) {
+    ARENA = arena;
+    ARENA_SIZE = size;
+    registered = 0;
+
+    return GFX_SUCCESS;
+}
+
+gfx_sprite* sprites_register(gfx_sprite sprite) {
+    if(registered >= (ARENA_SIZE - 1)) return NULL;
+
+    gfx_sprite* s = &ARENA[registered];
+    memcpy(s, &sprite, sizeof(gfx_sprite));
+    registered++;
+
+    return s;
+}
+
+gfx_error sprites_render(gfx_context* ctx) {
+    return gfx_sprite_render_array(ctx, 0, ARENA, registered);
+}
