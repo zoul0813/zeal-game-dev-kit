a79449702df9385db9d71a0e6d1aace31d1a5ce5
David Higgins <zoul0813@me.com>
2026-05-03 16:43:29 -0400

    fix bug in controller init after emulator reset, poll a few times before deciding the controller is not connected if controller caused reset

--- src/input/input.c
+++ src/input/input.c
@@ -2,43 +2,55 @@
 
 static uint8_t controller_mode = 0;
 
-zos_err_t input_init(uint8_t use_controller) {
+zos_err_t input_init(uint8_t use_controller)
+{
     // init and flush the keyboard
     zos_err_t err = keyboard_init();
-    if(err != ERR_SUCCESS) return err;
+    if (err != ERR_SUCCESS)
+        return err;
 
     err = keyboard_flush();
-    if(err != ERR_SUCCESS) return -err;
+    if (err != ERR_SUCCESS)
+        return -err;
 
-    if(!use_controller)
+    controller_mode = 0;
+
+    if (!use_controller)
         return ERR_SUCCESS;
-
-    // assume success, and enable controller
-    controller_mode = 1;
 
     // init and test the controller
     err = controller_init();
-    if(err != ERR_SUCCESS) {
-        controller_mode = 0;
+    if (err != ERR_SUCCESS) {
         return ERR_BAD_MODE;
     }
 
     err = controller_flush();
-    if(err != ERR_SUCCESS) {
-        controller_mode = 0;
+    if (err != ERR_SUCCESS) {
         return ERR_ENTRY_CORRUPTED;
     }
 
-    uint16_t test = controller_read();
-    // if unconnected, we'll get back 0xFFFF (all buttons pressed)
-    if (test & 0xFFFF) {
-        controller_mode = 0;
+    // Throw away first read after PIO setup
+    (void) controller_read();
+
+
+    uint8_t absent = 1;
+    for (uint8_t i = 0; i < 4; i++) {
+        uint16_t test = controller_read();
+        // if unconnected, we'll get back 0xFFFF (all buttons pressed)
+        if (test & 0xFFFF) {
+            absent = 0;
+            break;
+        }
     }
+
+    controller_mode = absent ? 0 : 1;
+    controller_flush();
 
     return ERR_SUCCESS;
 }
 
-uint16_t input_get(void) {
+uint16_t input_get(void)
+{
     uint16_t i = keyboard_read();
     if (controller_mode == 1) {
         i |= controller_read();
@@ -47,9 +59,10 @@ uint16_t input_get(void) {
     return i;
 }
 
-void input_flush(void) {
+void input_flush(void)
+{
     keyboard_flush();
-    if(controller_mode) {
+    if (controller_mode) {
         controller_flush();
     }
 }
